0d3e0504531ebcfc13626a1148f914d1c4ff7493
yosiasz <yosiasz@gmail.com>
2019-07-29 08:46:16 -0700

    heroes table SQL script

---
+++ src/server/sqlscript/005.heroes.tables.sql
@@ -0,0 +1,26 @@
+--heroes database needs to exist on your SQL Server
+use heroes
+go
+
+if exists(select 1 from master.sys.databases where name = 'heroes')
+    BEGIN
+        if not exists(select 1 from sys.tables where name = 'heroes')
+            BEGIN
+                create table heroes(id int not null identity(1,1), name nvarchar(150));
+
+                insert into heroes
+                select 'Mr. Nice'  union 
+                select 'Narco'  union
+                select 'Bombasto' union
+                select 'Celeritas' union
+                select 'Magneta' union
+                select 'RubberMan' union
+                select 'Dynama' union
+                select 'Dr IQ' union
+                select 'Magma' union
+                select 'Tornado'      
+            end
+    end
+else
+	PRINT 'You need to first create a heroes database'
+    go
