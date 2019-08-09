26950ff402f50c71092dbb254c6a76f4b7959e26
Josiah <jsolomon@meteorcomm.com>
2019-08-09 08:50:50 -0700

    Added redis caching

--- src/server/config/mssql.js
+++ src/server/config/mssql.js
@@ -1,11 +1,18 @@
 var config = {};
 
 var config = {
-		user: 'heroes',
-        password: '340$Uuxwp7Mcxo7Khy',
-        server: 'localhost',
-        database: 'heroes',
-        requestTimeout : 60000
-    }
+        redisConf: {
+            host: 'xxx.xxx.xxx.xxx', 
+            port: '6379'
+        },
+        sqlConf: {
+            user: 'heroes',
+            password: '340$Uuxwp7Mcxo7Khy',
+            server: 'localhost',
+            database: 'heroes',
+            requestTimeout : 60000,
+    
+        }
+    };      
 
-module.exports = config;    
+module.exports = config;   
--- src/server/routes/api/heroes.js
+++ src/server/routes/api/heroes.js
@@ -3,34 +3,52 @@ const uuid = require('uuid');
 const router = express.Router();
 const sql = require('mssql')
 const config = require('../../config/mssql')
+const redis = require('redis');
+const client = redis.createClient(config.redisConf);
+
+client.on('error', (err) => {
+  console.log("Error " + err);
+});
+
 
 // Gets All heroes
 router.get('/', (req, res) => {
 
-    for (var key in req.body) {
-      if(req.body[key] === '' ) {
-          req.body[key] = null;
-          }
-    }
-    
-    const pool2 = new sql.ConnectionPool(config, err => {
-      // ... error checks
-      pool2.on('error', err => {
-          console.log('ConnectionPool', err);
-      })
-    
-      pool2.request() //
-      .execute('dbo.heroes_sp', (err, result) => {
+  for (var key in req.body) {
+    if(req.body[key] === '' ) {
+        req.body[key] = null;
+        }
+  }
+  
+  const pool2 = new sql.ConnectionPool(config.sqlConf, err => {
+    return client.get(`getHeroes`, (err, result) => {
+      if (result) {
+        console.log('Data from redis:', result);
+        const resultJSON = JSON.parse(result);
+        return res.status(200).json(resultJSON);
+      } else {
+        
           // ... error checks
-          if (err){
+          pool2.on('error', err => {
             console.log('ConnectionPool', err);
+            })
+      
+        pool2.request() //
+        .execute('dbo.heroes_sp', (err, result) => {
+            // ... error checks
+            if (err){
+              console.log('ConnectionPool', err);
+            } else {
+              client.setex(`getHeroes`, 3600, JSON.stringify({ source: 'Redis Cache', ...result.recordsets[0], }));
+              res.send(result.recordsets[0])
+            }
+        })          
 
-          } else {
-            res.send(result.recordsets[0])
-
-          }
-      })
+      } //else redis data not found
     })
+
+
+  })
 
 })
 
@@ -43,7 +61,7 @@ router.get('/:id', (req, res) => {
         }
   }
   
-  const pool2 = new sql.ConnectionPool(config, err => {
+  const pool2 = new sql.ConnectionPool(config.sqlConf, err => {
     pool2.on('error', err => {
         console.log('ConnectionPool', err);
     })
@@ -72,7 +90,7 @@ router.post('/', (req, res) => {
     return res.status(400).json({ msg: 'Please include a name and email' });
   }
   
-  const pool = new sql.ConnectionPool(config, err => {
+  const pool = new sql.ConnectionPool(config.sqlConf, err => {
     // ... error checks
     pool.on('error', err => {
         console.log('ConnectionPool', err);
@@ -91,7 +109,7 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {  
   const updheroe = req.body;
 
-  const pool = new sql.ConnectionPool(config, err => {
+  const pool = new sql.ConnectionPool(config.sqlConf, err => {
     // ... error checks
     pool.on('error', err => {
         console.log('ConnectionPool', err);
@@ -120,7 +138,7 @@ router.put('/:id', (req, res) => {
 
 // Delete heroe
 router.delete('/:id', (req, res) => {
-  const pool = new sql.ConnectionPool(config, err => {
+  const pool = new sql.ConnectionPool(config.sqlConf, err => {
     // ... error checks
     pool.on('error', err => {
         console.log('ConnectionPool', err);
